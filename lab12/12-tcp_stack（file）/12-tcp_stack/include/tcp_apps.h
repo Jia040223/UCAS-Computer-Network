119fbb5738a28ec891d1b9d19e34bb245e7190e3
Jia040223 <1142747996@qq.com>
2023-12-03 16:41:42 +0800

    before lab12

---
+++ lab12/12-tcp_stack（file）/12-tcp_stack/include/tcp_apps.h
@@ -0,0 +1,8 @@
+#ifndef __TCP_APPS_H__
+#define __TCP_APPS_H__
+
+void *tcp_server(void *arg);
+void *tcp_client(void *arg);
+void *tcp_server_file(void *arg);
+void *tcp_client_file(void *arg);
+#endif
